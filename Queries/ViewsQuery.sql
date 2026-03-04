99ece74dcf0c8b4940f3e9fe478d2eaa96059faf
Laxmi Varshitha J <varshithajuturu2006@gmail.com>
2026-03-04 11:54:45 +0530

    Update and rename SQLQueries.sql to Queries/ViewsQuery.sql

--- Queries/ViewsQuery.sql
+++ Queries/ViewsQuery.sql
@@ -4,4 +4,5 @@ create View vw_ChurnData as
 Select * from prod_Churn where Customer_Status In('Churned' , 'Stayed')
 
 create View vw_JoinData  as 
+
 Select * from prod_Churn where Customer_Status ='Joined'
